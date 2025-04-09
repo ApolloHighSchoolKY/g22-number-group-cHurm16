c0fd6fc9a75f29384dc5c75247738743b51f8259
cHurm16 <chloe.hurm@stu.daviess.kyschools.us>
2025-04-09 13:29:38 +0000

    frq

--- MultipleGroups.java
+++ MultipleGroups.java
@@ -1,1 +1,13 @@
-
+public boolean contains(int number)
+{
+    for(NumberGroup group : groupList)
+    {
+    if(group.contains(number))
+    {
+    return true;
+    {
+    return false;
+    }
+    }
+    }
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,14 @@
-
+public class Range implements NumberGroup 
+{
+    private int minValue, maxValue; 
+    
+    public Range(int minValue, int maxValue)
+    {
+        this.minValue = minValue;
+        this.maxValue= maxValue;
+    }
+    public boolean contains(int value)
+    {
+        return minValue <= value && value <= maxValue;
+    }
+}
